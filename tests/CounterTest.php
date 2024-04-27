4b8f7efb6f9f1d235c1f95a4e74a3ee88ae1e053
Christian <kristian2015ok@gmail.com>
2024-04-27 14:38:21 +0700

    Add: Assertions

--- tests/CounterTest.php
+++ tests/CounterTest.php
@@ -3,13 +3,21 @@
 namespace Christian\Test;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
 
 class CounterTest extends TestCase 
 {
     public function testCounter()
     {
         $counter = new Counter();
+
         $counter->increment();
-        echo $counter->getCounter() . PHP_EOL;
+        Assert::assertEquals(1, $counter->getCounter());
+
+        $counter->increment();
+        $this->assertEquals(2, $counter->getCounter());
+
+        $counter->increment();
+        self::assertEquals(3, $counter->getCounter());
     }
 }
