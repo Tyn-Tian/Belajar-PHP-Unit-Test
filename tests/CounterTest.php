87ca7342bfee3c276cd9495a57be017fa3f84287
Christian <kristian2015ok@gmail.com>
2024-04-27 15:03:43 +0700

    Add: Test Dependency

--- tests/CounterTest.php
+++ tests/CounterTest.php
@@ -5,6 +5,7 @@ namespace Christian\Test;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Assert;
 use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\Depends;
 
 class CounterTest extends TestCase 
 {
@@ -29,5 +30,21 @@ class CounterTest extends TestCase
 
         $counter->increment();
         $this->assertEquals(1, $counter->getCounter());
+    }
+
+    public function testFirst(): Counter
+    {
+        $counter = new Counter();
+        $counter->increment();
+        $this->assertEquals(1, $counter->getCounter());
+
+        return $counter;
+    }
+
+    #[Depends('testFirst')]
+    public function testSecond(Counter $counter)
+    {
+        $counter->increment();
+        $this->assertEquals(2, $counter->getCounter());
     }
 }
