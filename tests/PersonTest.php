fb810ed0e722c9a84eaf97e2f8aad651335770e1
Christian <kristian2015ok@gmail.com>
2024-04-27 21:01:33 +0700

    Add: Test Exception

---
+++ tests/PersonTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Christian\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class PersonTest extends TestCase
+{
+    public function testSuccess()
+    {
+        $person = new Person("Christian");
+        self::assertEquals("Hi Budi, my name is Christian", $person->sayHello("Budi"));
+    }
+
+    public function testException() 
+    {
+        $person = new Person("Christian");
+        $this->expectException(\Exception::class);
+        $person->sayHello(null);
+    }
+}
